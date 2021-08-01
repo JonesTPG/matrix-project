08d71391142e6c96effd31c3d4592cc405d01ffb
JonesTPG <joonas.ryynanen1@gmail.com>
2021-08-01 11:07:30 +0300

    initial commit.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
+import matrix from './matrix.png';
+import button from './button.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={matrix} className="image" alt="matrix" />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img src={button} alt="button" />        
         </a>
       </header>
     </div>
